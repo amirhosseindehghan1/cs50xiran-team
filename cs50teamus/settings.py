c830f6b2e1df72d637bbed6025cd792769c035d6
amirhosseindehghan1 <dehghanamirhossein1@gmail.com>
2022-09-07 00:23:07 +0430

    allow host set *

--- cs50teamus/settings.py
+++ cs50teamus/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-hzt9stimg2j#j75u#-7v(1w8g3cg+g98!*^ep0r)49nnuk+l9l
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
